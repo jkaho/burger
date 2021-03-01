40765a1e8304ca79a04e1ba41795318cece88a8b
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-02 00:14:15 +1100

    Add seeds.sql file, modify 'seeds.sql' to add burgers table values

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Fish Burger');
