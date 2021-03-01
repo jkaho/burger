729e0066f070d6fa1535333acc569a6e731afe6a
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-02 00:49:01 +1100

    Modify 'burger.js' to create & export variables to call orm methods

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,9 @@
 const orm = require('../config/orm');
+
+const selectAll = orm.selectAll(burgers);
+const insertOne = orm.insertOne(burgers, burger_name, user_input);
+const updateOne = orm.updateOne(burgers, burger_name, user_input);
+
+module.exports = selectAll;
+module.exports = insertOne;
+module.exports = updateOne;
