8764b2ea2814c929a97540f25d4cf731d8c4e024
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-02 17:27:01 +1100

    Modify 'orm.js' to add callback functions to orm methods

--- config/orm.js
+++ config/orm.js
@@ -1,30 +1,30 @@
 const connection = require('./connection.js');
 
 const orm = {
-    selectAll(tableName) {
+    selectAll(tableName, cb) {
         const query = 'SELECT * FROM ??';
-        connection.query(query, [tableName],
+        connection.query(query, [tableName, cb],
             (err, result) => {
                 if (err) throw err;
-                console.log(result);
+                cb(result);
             }
         );
     },
-    insertOne(tableName, colName, value) {
+    insertOne(tableName, colName, value, cb) {
         const query = 'INSERT INTO ?? (??) VALUES (?)';
         connection.query(query, [tableName, colName, value],
             (err, result) => {
                 if (err) throw err;
-                console.log(result);
+                cb(result);
             }
         );
     },
-    updateOne(tableName, colName, value) {
+    updateOne(tableName, colName, value, cb) {
         const query = 'UPDATE ?? SET ?? = ?';
         connection.query(query, [tableName, colName, value],
             (err, result) => {
                 if (err) throw err;
-                console.log(result);
+                cb(result);
             }
         );
     }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { selectAll, insertOne, updateOne } = require('../models/burger');
 
 const router = (app) => {
-
+    app.get('/burgers', (req, res) => {
+        
+    })
 }
 
 module.exports = router;
