bc1eeeefbb539f91f2a1660344ad1784a5ca6668
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-03 19:53:03 +1100

    Modify 'schema.sql' to change default value of devoured col to 0

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50),
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN DEFAULT 0,
     PRIMARY KEY(id)
 );
 
